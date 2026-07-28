9e4353b3208d0101cc70c687b1c0320add77e352
Brandon <brandon@Brandons-MacBook-Pro.local>
2026-07-27 23:07:09 -0400

    Continue work on main loop, add stub for turning

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # turtlescripts
+## helper ideas
+- inpsect a specified number of blocks away
+- move a specified number of blocks away
+- wait/sleep function
+- check if item is in table
--- farming_turtle/farm.lua
+++ farming_turtle/farm.lua
@@ -1,5 +1,7 @@
 MOVE_SLEEP_TIME = 0
---     sleep between moves
+SEED_IDS = {"minecraft:wheat_seeds","minecraft:melon_seeds","minecraft:pumpkin_seeds","minecraft:beetroot_seeds"}
+PLANT_IDS = {"minecraft:wheat", "minecraft:melon", "minecraft:pumpkin", "minecraft:beetroots"}
+--     sleep between moves TODO will have to write a helper for this
 --     if there's nothing two blocks down, turn around and come back down next column
 --     if there's nothing underneath two blocks down for two blocks, turn to the left
 --     if the crop is folly grown, digDown and suck!
@@ -7,31 +9,74 @@ MOVE_SLEEP_TIME = 0
 --     if the inventory is full, break the loop
 
 -- basic setup, refeuling and whatnot
+-- TODO may want to include refueling in main loop
 turtle.select(1)
 turtle.refuel()
 
 -- hover one block above the gorund 
 -- TODO unsure if plants are considered "solid" blocks
-if (!turtle.detectDown()) then
+if not turtle.detectDown() then
     turtle.up()
+end
+
+-- helper to check if item in list
+function in_table(item, table)
+    for i, v in ipairs(table) do
+        if item == v then
+            return true
+        end
+    end
+    return false
 end
 
 -- select and plant a seed
 function plant_seed()
+    -- select the seed
+    for i = 1, 16 do
+        block = turtle.getItemDetail(i)
+        if block and in_table(block.name, SEED_IDS) then
+            turtle.select(i)
+            break
+        end
+    end
+    -- plant it
+    return turtle.placeDown()
+end
+
+-- turn around to the left
+function turn_around_left()
 end
 
 -- main loop, keep moving until you're just sooooo full
 while true do
+    -- move forward one
+    turtle.forward()
     -- look undernearth for crop
-    block_below = turtle.inspectDown()
-    -- check if the crop is wheat
-    if (block_below.name == "minecraft:wheat") then
+    local has_block_below, block_below = turtle.inspectDown()
+    -- check if the black is a plant
+    if in_table(block_below.name, PLANT_IDS) then
         -- check if the wheat is age 7 (fully grown)
-        if (block_below.age == 7) then
+        if block_below.age == 7 then
             -- break and collect the block
             turtle.digDown()
             turtle.suck()
             -- find and plant a new seed
+            ::plant::
+            if plant_seed() then
+                goto continue
+            end
+            -- till ground
+            turtle.down()
+            turtle.digDown()
+            turtle.up()
+            goto plant
         end
     end
+    -- if the block below does not exist move down one
+    if not has_block_below then
+        turtle.down()
+        local has_block_below, block_below = turtle.inspectDown()
+        -- if the block below is not a tilled dirt block, turn around
+    end
+    ::continue::
 end
