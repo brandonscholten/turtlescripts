2f307181401009ff029a274e46c548774ce2b3b8
Brandon <brandonscholten@murena.io>
2026-08-19 20:28:49 -0400

    Debugging

--- farming_turtle/farm.lua
+++ farming_turtle/farm.lua
@@ -1,7 +1,8 @@
 MOVE_SLEEP_TIME = 0
 SEED_IDS = {"minecraft:wheat_seeds","minecraft:melon_seeds","minecraft:pumpkin_seeds","minecraft:beetroot_seeds"}
-PLANT_IDS = {"minecraft:wheat", "minecraft:melon", "minecraft:pumpkin", "minecraft:beetroots"}
+PLANT_IDS = {"minecraft:wheat", "minecraft:melon", "minecraft:pumpkin", "minecraft:beetroots", "minecraft:potatoes"}
 FARMLAND_ID = "minecraft:farmland"
+DIRT_ID = "minecraft:dirt"
 --     sleep between moves TODO will have to write a helper for this
 --     if there's nothing two blocks down, turn around and come back down next column
 --     if there's nothing underneath two blocks down for two blocks, turn to the left
@@ -13,10 +14,11 @@ FARMLAND_ID = "minecraft:farmland"
 -- TODO may want to include refueling in main loop
 turtle.select(1)
 turtle.refuel()
+print(turtle.getFuelLevel())
 
 -- hover one block above the gorund 
 -- TODO unsure if plants are considered "solid" blocks
-if not turtle.detectDown() then
+if turtle.detectDown() then
     turtle.up()
 end
 
@@ -47,40 +49,41 @@ end
 -- main loop, keep moving until you're just sooooo full
 while true do
     -- move forward one
-    turtle.forward()
+    print("moving forward")
+    print(turtle.forward())
     -- look undernearth for crop
     local has_block_below, block_below = turtle.inspectDown()
     -- check if the black is a plant
+    print(block_below.name)
+    print(in_table(block_below.name, PLANT_IDS))
     if in_table(block_below.name, PLANT_IDS) then
-        -- check if the wheat is age 7 (fully grown)
+        -- check if the plant is age 7 (fully grown)
         if block_below.age == 7 then
             -- break and collect the block
             turtle.digDown()
             turtle.suck()
             -- find and plant a new seed
-            ::plant::
-            if plant_seed() then
-                goto continue
+            while (not plant_seed()) do
+                -- till ground and try again until a seed is planted
+                turtle.digDown()
             end
-            -- till ground
-            turtle.down()
-            turtle.digDown()
-            turtle.up()
-            goto plant
         end
     end
     -- if the block below does not exist move down one
     if not has_block_below then
-        turtle.down()
         local has_block_below, block_below = turtle.inspectDown()
         -- if the block below is not a tilled dirt block, turn around
         if block_below.name == FARMLAND_ID then
             plant_seed()
             turtle.up()
         else
-            turtle.left()
-            turtle.up()
+            -- if the black below is an un-tilled dirt block, till it
+            if block_below.name == DIRT_ID then
+                turtle.digDown()
+            else
+                turtle.turnLeft()
+                turtle.up()
+            end
         end
     end
-    ::continue::
 end
